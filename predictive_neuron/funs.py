74d8a157aa47e731fc78cbf57257b59abe40bb43
Matteo Saponati <matteosaponati@gmail.com>
2022-04-06 18:44:34 +0200

    add get_input funs with noise components

--- predictive_neuron/funs.py
+++ predictive_neuron/funs.py
@@ -14,6 +14,7 @@ Author:
 ----------------------------------------------
 """
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -26,22 +27,60 @@ def get_sequence(par,timing):
                                 for i in range(par.T)]).view(1,1,-1).float()
     x_data = F.conv1d(x_data.permute(0,2,1),filter.expand(par.N,-1,-1),
                          padding=par.T,groups=par.N)[:,:,1:par.T+1]
+
+    return x_data.permute(0,2,1)
+    
+def get_sequence_noise(par,timing,mu=None,jitter=None):
+        
+    'background firing noise'
+    if mu:
+        prob = par.freq*par.dt
+        mask = torch.rand(par.batch,par.T,par.N).to(par.device)
+        x_data = torch.zeros(par.batch,par.T,par.N).to(par.device)
+        x_data[mask<prob] = 1
+        x_data[:,timing,range(par.N)] = 1
+    else:
+        x_data = torch.zeros(par.batch,par.T,par.N).to(par.device)
+        x_data[:,timing,range(par.N)] = 1
+    
+    'jitter in sequence'
+    if jitter:
+         for b in range(par.batch):
+             timing_err = np.array(timing) + (np.random.randint(-par.jitter,par.jitter,par.N))/par.dt
+             x_data[b,timing_err.tolist(),range(par.N)] = 1
+            
+    filter = torch.tensor([(1-par.dt/par.tau_x)**(par.T-i-1) 
+                                for i in range(par.T)]).view(1,1,-1).float()
+    x_data = F.conv1d(x_data.permute(0,2,1),filter.expand(par.N,-1,-1),
+                         padding=par.T,groups=par.N)[:,:,1:par.T+1]
     
     return x_data.permute(0,2,1)
 
-def get_sequence_noise(par,timing):
+def get_sequence_capacity(par,timing):
+    
+    'define the set of sequences'
+    seq = [[a,b] for a in range(par.N) for b in range(par.N) if a!=b]
+    
+    x_data = torch.zeros(par.batch,par.T,par.N).to(par.device)    
+    for k in range(par.batch):
+        x_data[k,timing,seq[k]] = 1
+    
+    filter = torch.tensor([(1-par.dt/par.tau_x)**(par.T-i-1) 
+                                for i in range(par.T)]).view(1,1,-1).float()
+    x_data = F.conv1d(x_data.permute(0,2,1),filter.expand(par.N,-1,-1),
+                         padding=par.T,groups=par.N)[:,:,1:par.T+1]
+    
+    return x_data.permute(0,2,1)
 
-    prob = par.freq*par.dt
-    mask = torch.rand(par.batch,par.T,par.N).to(par.device)
-    x_data = torch.zeros(par.batch,par.T,par.N).to(par.device)
-    x_data[mask<prob] = 1.0
+
+def get_sequence_capacity2(par,timing,step):
     
-    x_data[:,timing,range(par.N)] = 1
+    x_data = torch.zeros(par.batch,par.T,par.N).to(par.device)   
+    count = 0 
+    for k in range(par.batch):
+        x_data[k,timing[:step],count+np.arange(int(step))] = 1
+        count += step
     
-    # for b in range(par.batch):
-    #     # timing_err = np.array(timing) + np.random.randint(0,2,par.N)
-    #     x_data[b,timing.tolist(),range(par.N)]
-        
     filter = torch.tensor([(1-par.dt/par.tau_x)**(par.T-i-1) 
                                 for i in range(par.T)]).view(1,1,-1).float()
     x_data = F.conv1d(x_data.permute(0,2,1),filter.expand(par.N,-1,-1),
