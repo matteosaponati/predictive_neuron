d533d6183528c71330cb1ae631ab21108cbf753b
Matteo Saponati <matteosaponati@gmail.com>
2022-06-21 18:44:42 +0200

    neural network test

--- nn/nn_test.py
+++ nn/nn_test.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
 plt.rc('axes', axisbelow=True)
 
-from predictive_neuron import models_nn, funs
-
 '----------------'
 def forward(par,network,x_data):
     
@@ -28,12 +26,68 @@ def forward(par,network,x_data):
 #%%
 
 
+import numpy as np
+import torch
+import torch.nn as nn
 
+def set_device():
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  return device
 
+'------------------'
 
+class NetworkClass(nn.Module):
+    """
+    NETWORK MODEL
+    - get the input vector at the current timestep
+    - compute the current prediction error
+    - update the recursive part of the gradient and the membrane potential
+    - thresholding nonlinearity and evaluation of output spike
+    inputs:
+        par: model parameters
+    """
+    
+    def __init__(self,par):
+        super(NetworkClass,self).__init__() 
+        
+        self.par = par  
+        self.alpha = (1-self.par.dt/self.par.tau_m)  
+        self.beta = (1-self.par.dt/self.par.tau_x)              
+        
+        self.w = nn.Parameter(torch.empty((self.par.n_in,self.par.nn)).to(self.par.device))
+        torch.nn.init.normal_(self.w, mean=0.0, std=1/np.sqrt(self.par.n_in))
+        
+        if self.par.is_rec == 'True':
+            w_rec = np.random.randn(self.par.nn,self.par.nn)/np.sqrt(self.par.nn)
+            w_rec = np.where(np.eye(self.par.nn)>0,np.zeros_like(w_rec),w_rec)
+            self.wrec = nn.Parameter(torch.as_tensor(w_rec,dtype=self.par.dtype).to(self.par.device))
+        
+    def state(self):
+        """initialization of network state"""
+        
+        self.v = torch.zeros(self.par.N,self.par.nn).to(self.par.device)
+        self.z = torch.zeros(self.par.N,self.par.nn).to(self.par.device)
+        self.z_out = torch.zeros(self.par.N,self.par.nn).to(self.par.device)
+        
+    def __call__(self,x):
+        
+        print(self.v.shape)
+        'update membrane voltages'
+        self.v = self.alpha*self.v + x@self.w \
+                     - self.par.v_th*self.z.detach()
+        
+                    
+#        self.v = self.alpha*self.v + torch.sum(torch.multiply(x,self.w),dim=1) \
+#                    - self.par.v_th*self.z.detach()
+        if self.par.is_rec == 'True': 
+            self.z_out = self.beta*self.z_out + self.z.detach()
+            self.v += self.z_out.detach()@self.wrec
+        
+        'update output spikes'
+        self.z = torch.zeros(self.par.N,self.par.nn).to(self.par.device)
+        self.z[(self.v-self.par.v_th>0)[0,:]] = 1
 
-
-
+#%%
 
 
 savedir = ''
@@ -41,11 +95,12 @@ savedir = ''
 par = types.SimpleNamespace()
 
 'architecture'
-par.N = 2
+par.N = 1
+par.n_in = 2
 par.nn = 2
-par.T = 600
+par.T = 200
 par.batch = 1
-par.epochs = 100
+par.epochs = 2
 par.device = 'cpu'
 par.dtype = torch.float
 
@@ -58,24 +113,46 @@ par.tau_x = 2.
 
 par.is_rec = False
 
+#%%
 'set inputs'
 timing = np.array([2.,6.])/par.dt
 par.Dt = 4
 timing = np.cumsum(np.random.randint(0,par.Dt,par.N))/par.dt
 x_data  = []
+
+import torch.nn.functional as F
+
+def get_sequence(par,timing):
+
+    x_data = torch.zeros(par.batch,par.T,par.N).to(par.device)
+    x_data[:,timing,range(par.N)] = 1
+            
+    'synaptic time constant'
+    filter = torch.tensor([(1-par.dt/par.tau_x)**(par.T-i-1) 
+                                for i in range(par.T)]).view(1,1,-1).float().to(par.device) 
+    x_data = F.conv1d(x_data.permute(0,2,1),filter.expand(par.N,-1,-1),
+                         padding=par.T,groups=par.N)[:,:,1:par.T+1]
+    
+    return x_data.permute(0,2,1)
+
+#%%
+
 for n in range(par.nn):
-    x,_ = funs.get_sequence(par,timing + n*(4/par.dt))
+    x = get_sequence(par,timing + n*(4/par.dt))
     print(timing + n*(4/par.dt))
     x_data.append(x)
+
 x_data = torch.stack(x_data,dim=3)
+
+#%%
     
 # x_data,density = funs.get_sequence(par,timing)
 
 'set model'
-network = models_nn.NetworkClass(par)
+network = NetworkClass(par)
 par.w_0 = .03
 par.w_0rec = .0
-network.w = nn.Parameter(par.w_0*torch.ones(par.N,par.nn)).to(par.device)
+network.w = nn.Parameter(par.w_0*torch.ones(par.n_in,par.nn)).to(par.device)
 if par.is_rec == True: 
     w_rec=  par.w_0rec*np.ones((par.nn,par.nn))
     w_rec = np.where(np.eye(par.nn)>0,np.zeros_like(w_rec),w_rec)
