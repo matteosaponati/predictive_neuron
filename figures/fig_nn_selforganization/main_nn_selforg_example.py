51f542ab4dc6a1605a13707a4aaacf859fafd9ad
matteosaponati <matteosaponati@gmail.com>
2022-10-17 17:03:19 +0200

    updated nn selforg example

--- figures/fig_nn_selforganization/main_nn_selforg_example.py
+++ figures/fig_nn_selforganization/main_nn_selforg_example.py
@@ -39,23 +39,22 @@ par.eta = 1e-5
 par.tau_m = 20.
 par.v_th = 3.
 par.tau_x = 2.
-par.nn = 8
+par.nn = 4
 par.lateral = 2
 par.is_rec = True
-par.bound = 'none'
 
 'set noise sources'
-par.noise = False
+par.noise = True
 par.freq_noise = True
 par.freq = 10
 par.jitter_noise = True
-par.jitter = 2
+par.jitter = 1
 par.batch = 1
 
 'set input'
 par.sequence = 'deterministic'
 par.Dt = 2
-par.n_in = 26
+par.n_in = 10
 par.delay = 4
 timing = [[] for n in range(par.nn)]
 spk_times = np.linspace(par.Dt,par.Dt*par.n_in,par.n_in)/par.dt
@@ -64,27 +63,92 @@ for n in range(par.nn):
 
 'set initialization and training algorithm'
 par.init = 'fixed'
-par.init_mean = 0.08
-par.init_a, par.init_b = 0, .12
-par.w_0rec = .003
+par.init_mean = 0.03
+par.init_a, par.init_b = 0, .06
+par.w_0rec = .000
 
 'set training algorithm'
-par.bound = 'soft'
-par.epochs = 10
+par.bound = 'none'
+par.epochs = 400
 
 'set noise sources'
-par.noise = False
-par.freq_noise = True
-par.freq = 1
-par.jitter_noise = True
-par.jitter = 1
 par.T = int((par.n_in*par.delay + par.n_in*par.Dt + par.jitter + 80)/par.dt)
 
 #%%
 x = funs.get_sequence_nn_selforg_NumPy(par,timing)
 
+#%%
+plt.imshow(x[:,:,-1],aspect='auto')
+
+#%%
+       
+class NetworkClass_SelfOrg_NumPy():
+    """
+    NETWORK MODEL
+    - get the input vector at the current timestep
+    - compute the current prediction error
+    - update the recursive part of the gradient and the membrane potential
+    - thresholding nonlinearity and evaluation of output spike
+    inputs:
+        par: model parameters
+    """
+    
+    def __init__(self,par):
+        
+        self.par = par  
+        self.alpha = (1-self.par.dt/self.par.tau_m)  
+        self.beta = (1-self.par.dt/self.par.tau_x)              
+        self.w = np.zeros((self.par.n_in+self.par.lateral,self.par.nn))
+        
+    def state(self):
+        """initialization of network state"""
+
+        self.v = np.zeros(self.par.nn)
+        self.z = np.zeros(self.par.nn)
+        self.z_out = np.zeros(self.par.nn)
+        'external inputs + lateral connections'
+        self.p = np.zeros((self.par.n_in+2,self.par.nn))
+        self.epsilon = np.zeros((self.par.n_in+2,self.par.nn))
+        self.grad = np.zeros((self.par.n_in+2,self.par.nn))  
+
+    def __call__(self,x):
+        
+        'create total input'
+        self.z_out = self.beta*self.z_out + self.z
+        
+        x_tot = np.zeros((self.par.n_in+2,self.par.nn))
+        for n in range(self.par.nn): 
+            if n == 0:
+                x_tot[:,n] = np.concatenate((x[:,n],np.array([0,self.z_out[n+1]])),axis=0)       
+            elif n == self.par.nn-1:
+                x_tot[:,n] = np.concatenate((x[:,n],np.array([self.z_out[n-1],0])),axis=0)   
+            else: 
+                x_tot[:,n] = np.concatenate((x[:,n],np.array([self.z_out[n-1],self.z_out[n+1]])),axis=0) 
+                
+        'compute prediction error (eq 4) and update parameters (eq 3)'
+        self.epsilon = x_tot - self.w*self.v
+        self.grad = self.v*self.epsilon \
+                         + np.sum(self.w*self.epsilon,axis=0)*self.p
+        self.p = self.alpha*self.p + x_tot
+        
+        'soft: apply soft lower-bound, update proportional to parameters'
+        'hard: apply hard lower-bound, hard-coded positive parameters'
+        
+        if self.par.bound == 'soft':
+            self.w = self.w + self.w*self.par.eta*self.grad
+        elif self.par.bound == 'hard':
+            self.w = self.w + self.par.eta*self.grad
+            self.w = np.where(self.w<0,np.zeros_like(self.w),self.w)
+        else: self.w = self.w + self.par.eta*self.grad
+                
+        'update membrane voltage (eq 1)'
+        self.v = self.alpha*self.v + np.sum(x_tot*self.w,axis=0) \
+                 - self.par.v_th*self.z
+        self.z = np.zeros(self.par.nn)
+        self.z[self.v-self.par.v_th>0] = 1
+
 'set model'
-network = models.NetworkClass_SelfOrg_NumPy(par)
+network = NetworkClass_SelfOrg_NumPy(par)
 network = funs_train.initialization_weights_nn_NumPy(par,network)
  
 #%%
@@ -94,10 +158,29 @@ network = funs_train.initialization_weights_nn_NumPy(par,network)
 w,v,spk = funs_train.train_nn_NumPy(par,network,x=x)
 
 
-#%%
+# w,v,spk = funs_train.train_nn_NumPy(par,network,timing=timing)
 
+
+ #%%
+
+m=1
+for n in range(par.nn):
+    plt.eventplot(spk[n][-1],lineoffsets = m,linelengths = 1,linewidths = 3,colors = 'rebeccapurple')
+    m+=1
+plt.ylim(1,par.nn)
+plt.xlim(0,par.T*par.dt)
+plt.xlabel('time [ms]')
+
+
+
+#%%
 plt.imshow(w[-1])
 plt.colorbar()
+
+
+#%%
+
+plt.plot(v[-1][-1,:])
 #%%
 
 w = np.vstack(w)
