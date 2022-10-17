7a3617df4e4ab4dde673ae65c1b7f3c43a8a0b4c
Matteo Saponati <matteosaponati@gmail.com>
2022-10-17 21:02:18 +0200

    updated main selforganization + moved into scripts

--- figures/fig_nn_selforganization/main_nn_selforg_example.py
+++ figures/fig_nn_selforganization/main_nn_selforg_example.py
@@ -35,26 +35,26 @@ par = types.SimpleNamespace()
 'set model'
 par.device = 'cpu'
 par.dt = .05
-par.eta = 1e-5
-par.tau_m = 20.
-par.v_th = 3.
+par.eta = 5e-5
+par.tau_m = 15.
+par.v_th = 4.
 par.tau_x = 2.
-par.nn = 4
+par.nn = 8
 par.lateral = 2
 par.is_rec = True
 
 'set noise sources'
 par.noise = True
 par.freq_noise = True
-par.freq = 10
+par.freq = 5
 par.jitter_noise = True
-par.jitter = 1
+par.jitter = 2
 par.batch = 1
 
 'set input'
 par.sequence = 'deterministic'
 par.Dt = 2
-par.n_in = 10
+par.n_in = 26
 par.delay = 4
 timing = [[] for n in range(par.nn)]
 spk_times = np.linspace(par.Dt,par.Dt*par.n_in,par.n_in)/par.dt
@@ -65,107 +65,52 @@ for n in range(par.nn):
 par.init = 'fixed'
 par.init_mean = 0.03
 par.init_a, par.init_b = 0, .06
-par.w_0rec = .000
+par.w_0rec = .0003
 
 'set training algorithm'
 par.bound = 'none'
-par.epochs = 400
+par.epochs = 500
 
 'set noise sources'
 par.T = int((par.n_in*par.delay + par.n_in*par.Dt + par.jitter + 80)/par.dt)
 
+'---------------------------------------------'
+
+"""
+there are three sources of noise for each epoch:
+    1. jitter of the spike times (random jitter between -par.jitter and +par.jitter)
+    2. random background firing following an homogenenous Poisson process with rate
+    distributione between 0 and par.freq 
+    3. another subset of N_dist pre-synaptic neurons that fire randomly according
+    to an homogenenous Poisson process with randomly distribuited rates between
+    0 and par.freq
+"""
 #%%
 x = funs.get_sequence_nn_selforg_NumPy(par,timing)
 
 #%%
-plt.imshow(x[:,:,-1],aspect='auto')
+plt.imshow(x[:,:,0],aspect='auto')
 
-#%%
-       
-class NetworkClass_SelfOrg_NumPy():
-    """
-    NETWORK MODEL
-    - get the input vector at the current timestep
-    - compute the current prediction error
-    - update the recursive part of the gradient and the membrane potential
-    - thresholding nonlinearity and evaluation of output spike
-    inputs:
-        par: model parameters
-    """
-    
-    def __init__(self,par):
-        
-        self.par = par  
-        self.alpha = (1-self.par.dt/self.par.tau_m)  
-        self.beta = (1-self.par.dt/self.par.tau_x)              
-        self.w = np.zeros((self.par.n_in+self.par.lateral,self.par.nn))
-        
-    def state(self):
-        """initialization of network state"""
-
-        self.v = np.zeros(self.par.nn)
-        self.z = np.zeros(self.par.nn)
-        self.z_out = np.zeros(self.par.nn)
-        'external inputs + lateral connections'
-        self.p = np.zeros((self.par.n_in+2,self.par.nn))
-        self.epsilon = np.zeros((self.par.n_in+2,self.par.nn))
-        self.grad = np.zeros((self.par.n_in+2,self.par.nn))  
-
-    def __call__(self,x):
-        
-        'create total input'
-        self.z_out = self.beta*self.z_out + self.z
-        
-        x_tot = np.zeros((self.par.n_in+2,self.par.nn))
-        for n in range(self.par.nn): 
-            if n == 0:
-                x_tot[:,n] = np.concatenate((x[:,n],np.array([0,self.z_out[n+1]])),axis=0)       
-            elif n == self.par.nn-1:
-                x_tot[:,n] = np.concatenate((x[:,n],np.array([self.z_out[n-1],0])),axis=0)   
-            else: 
-                x_tot[:,n] = np.concatenate((x[:,n],np.array([self.z_out[n-1],self.z_out[n+1]])),axis=0) 
-                
-        'compute prediction error (eq 4) and update parameters (eq 3)'
-        self.epsilon = x_tot - self.w*self.v
-        self.grad = self.v*self.epsilon \
-                         + np.sum(self.w*self.epsilon,axis=0)*self.p
-        self.p = self.alpha*self.p + x_tot
-        
-        'soft: apply soft lower-bound, update proportional to parameters'
-        'hard: apply hard lower-bound, hard-coded positive parameters'
-        
-        if self.par.bound == 'soft':
-            self.w = self.w + self.w*self.par.eta*self.grad
-        elif self.par.bound == 'hard':
-            self.w = self.w + self.par.eta*self.grad
-            self.w = np.where(self.w<0,np.zeros_like(self.w),self.w)
-        else: self.w = self.w + self.par.eta*self.grad
-                
-        'update membrane voltage (eq 1)'
-        self.v = self.alpha*self.v + np.sum(x_tot*self.w,axis=0) \
-                 - self.par.v_th*self.z
-        self.z = np.zeros(self.par.nn)
-        self.z[self.v-self.par.v_th>0] = 1
 
 'set model'
-network = NetworkClass_SelfOrg_NumPy(par)
+network = models.NetworkClass_SelfOrg_NumPy(par)
 network = funs_train.initialization_weights_nn_NumPy(par,network)
  
 #%%
 
 
 'training'
-w,v,spk = funs_train.train_nn_NumPy(par,network,x=x)
+# w,v,spk = funs_train.train_nn_NumPy(par,network,x=x)
+# 
 
-
-# w,v,spk = funs_train.train_nn_NumPy(par,network,timing=timing)
+w,v,spk = funs_train.train_nn_NumPy(par,network,timing=timing)
 
 
  #%%
 
 m=1
 for n in range(par.nn):
-    plt.eventplot(spk[n][-1],lineoffsets = m,linelengths = 1,linewidths = 3,colors = 'rebeccapurple')
+    plt.eventplot(spk[n][-5],lineoffsets = m,linelengths = 1,linewidths = 3,colors = 'rebeccapurple')
     m+=1
 plt.ylim(1,par.nn)
 plt.xlim(0,par.T*par.dt)
@@ -174,7 +119,7 @@ plt.xlabel('time [ms]')
 
 
 #%%
-plt.imshow(w[-1])
+plt.imshow(w[-10])
 plt.colorbar()
 
 
@@ -191,6 +136,56 @@ plt.xlabel(r'epochs')
 plt.xlim(0,par.epochs)
 plt.imshow(w.T,aspect='auto',cmap='coolwarm')#,norm=MidpointNormalize(midpoint=1))
 plt.colorbar()
+
+
+
+#%%
+'---------------------------------------------'
+'plots'
+
+'Panel c'   
+hex_list = ['#33A1C9','#FFFAF0','#7D26CD']
+fig = plt.figure(figsize=(6,6), dpi=300)    
+divnorm = colors.DivergingNorm(vmin=w[-1].min(),vcenter=0, vmax=w[-1].min())
+plt.imshow(w[-1],cmap=funs.get_continuous_cmap(hex_list), norm=divnorm,aspect='auto')
+fig.tight_layout(rect=[0, 0.01, 1, 0.97])
+plt.colorbar()
+plt.ylabel(r'inputs')
+plt.xlabel(r'neurons')
+plt.savefig('w_nn.png',format='png', dpi=300)
+plt.savefig('w_nn.pdf',format='pdf', dpi=300)
+plt.close('all')
+
+'Panel d'
+total_duration = np.zeros(par.epochs)
+for e in range(par.epochs):
+    if spk[-1][e] != [] and spk[0][e] != []:
+        total_duration[e] = spk[-1][e][-1] - spk[0][e][0]
+    else: total_duration[e-1]
+fig = plt.figure(figsize=(6,6), dpi=300)    
+plt.plot(total_duration,color='purple',linewidth=2)
+fig.tight_layout(rect=[0, 0.01, 1, 0.97])
+plt.colorbar()
+plt.ylabel(r'$\Delta t$ [ms]')
+plt.xlabel(r'epochs')
+plt.savefig('total_duration.png',format='png', dpi=300)
+plt.savefig('total_duration.pdf',format='pdf', dpi=300)
+plt.close('all')    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #%%
