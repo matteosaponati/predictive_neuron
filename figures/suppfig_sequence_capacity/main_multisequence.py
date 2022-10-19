bdd2254717f606f05e0cb4a33c1618891c85b7c2
matteosaponati <matteosaponati@gmail.com>
2022-10-19 16:21:05 +0200

    updated multisequence: batch learning with pytorch

--- figures/suppfig_sequence_capacity/main_multisequence.py
+++ figures/suppfig_sequence_capacity/main_multisequence.py
@@ -3,8 +3,11 @@
 Copyright (C) Vinck Lab
 -add copyright-
 ----------------------------------------------
-"main_multisequence.py"
-single neuron trained on multiple input sequences - Figure 2
+"fig2_capacity.py":
+    
+    - numerical investigation of the capacity of the model
+    - dependence on overlap between synapses
+    - dependence on model parameters
 
 Author:
     
@@ -14,110 +17,136 @@ Author:
 ----------------------------------------------
 """
 
-import torch
 import numpy as np
-    
-from predictive_neuron import models, funs, funs_train
+import os
+import types
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 22})
+plt.rc('axes', axisbelow=True)
+import matplotlib.colors as colors
+class MidpointNormalize(colors.Normalize):
+	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+		self.midpoint = midpoint
+		colors.Normalize.__init__(self, vmin, vmax, clip)
+	def __call__(self, value, clip=None):
+		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
-if __name__ == '__main__':
-    
-    import argparse
-    parser = argparse.ArgumentParser(
-                    description='single neuron trained on sequences'
-                    )
-    
-    parser.add_argument('--name',type=str, 
-                        choices=['sequence','multisequence'],default='multisequence',
-                        help='type of sequence inputs')
-    
-    'training algorithm'
-    parser.add_argument('--optimizer',type=str, 
-                        choices=['online','SGD','Adam'],default='Adam',
-                        help='choice of optimizer')
-    
-    parser.add_argument('--bound',type=str,
-                        choices=['None','hard','soft'],default='None',
-                        help='set hard lower bound for parameters')
-    
-    parser.add_argument('--init',type=str, 
-                        choices=['classic','random','fixed'],default='fixed',
-                        help='type of weights initialization')
-    
-    parser.add_argument('--init_mean',type=float, default=0.1)
-    parser.add_argument('--init_a',type=float, default=0.)
-    parser.add_argument('--init_b',type=float, default=.2)
+from predictive_neuron import models, funs_train
 
-    parser.add_argument('--epochs', type=int, default=400)
-    parser.add_argument('--seed', type=int, default=1992)
-    parser.add_argument('--batch', type=int, default=4)
-    parser.add_argument('--device', type=str, default="cpu")
-    parser.add_argument('--rep', type=int, default=1)
-    
-    'set input sequence'
-    parser.add_argument('--sequence',type=str, 
-                        choices=['deterministic','random'],default='random')
-    parser.add_argument('--Dt', type=int, default=4) 
-    parser.add_argument('--N_sub', type=int, default=5)
-    
-    'set noise sources'
-    parser.add_argument('--noise', type=bool, default=True)
-    parser.add_argument('--freq_noise', type=bool, default=True)
-    parser.add_argument('--freq', type=float, default=10) 
-    parser.add_argument('--jitter_noise', type=bool, default=True) 
-    parser.add_argument('--jitter', type=float, default=2)
-    parser.add_argument('--onset', type=bool, default=False)
-    
-    'neuron model'
-    parser.add_argument('--dt', type=float, default= .05) 
-    parser.add_argument('--tau_m', type=float, default= 10.) 
-    parser.add_argument('--v_th', type=float, default= 2.)
-    parser.add_argument('--eta',type=float, default=1e-3)
-    parser.add_argument('--tau_x', type=float, default= 2.)
-    parser.add_argument('--dtype', type=str, default=torch.float) 
-    
-    par = parser.parse_args()
-    
-    '-----------------'
-    
-    'set total length of simulation and total input size'
-    par.N = par.N_sub*par.batch
-    par.T = int(2*(par.Dt*par.N + par.jitter)/(par.dt))
-    par.N_subseq = [np.arange(k,k+par.N_sub) 
+savedir = '/gs/home/saponatim/'
+
+'set model'
+par = types.SimpleNamespace()
+par.epochs = 100
+par.device = 'cpu'
+par.dt = .05
+par.eta = 1e-3
+par.tau_m = 15.
+par.v_th = 1.5
+par.tau_x = 2.
+
+'set input'
+par.sequence = 'deterministic'
+par.Dt = 2
+par.N_sub = 10
+par.delay = 40
+par.batch = 4
+par.N = par.N_sub*par.batch 
+par.N_subseq = [np.arange(k,k+par.N_sub) 
                     for k in np.arange(0,par.N+par.N_sub,par.N_sub)]
+timing = []
+for b in range(par.batch):
+    timing.append(((np.linspace(par.Dt,par.Dt*par.N_sub,par.N_sub))/par.dt).astype(int))
+
+'set training algorithm'
+par.optimizer = 'Adam'
+par.bound = 'none'
+par.epochs = 500
+
+'set initialization'
+par.init = 'fixed'
+par.init_mean = 0.03
+par.init_a, par.init_b = 0, .06
+
+'set noise sources'
+par.name = 'multisequence'
+par.noise = True
+par.freq_noise = True
+par.freq = 10
+par.jitter_noise = True
+par.jitter = 2
+par.T = int((par.Dt*par.N + par.jitter)/(par.dt))
+
+'---------------------------------------------'
+
+'fix seed'
+np.random.seed(1992)
+
+'set model'
+neuron = models.NeuronClass(par)
+neuron = funs_train.initialize_weights_PyTorch(par,neuron)
+
+'training'
+w,v,spk,loss = funs_train.train_PyTorch(par,neuron,timing=timing)
+
+#%%
+'plots'
+
+'Pnale b'
+spk_plot = [[] for b in range(par.batch)]
+for b in range(par.batch):
     
-    'set timing'
-    timing = []
-    if par.sequence == 'deterministic':
-        for b in range(par.batch):
-            timing.append((np.linspace(
-                par.Dt,par.Dt*par.N_sub,par.N_sub)/par.dt).astype(int))
-    elif par.sequence == 'random':
-        for b in range(par.batch):
-            timing.append((np.cumsum(
-                np.random.randint(0,par.Dt,par.N_sub))/par.dt).astype(int))
-    
-    'fix seed'
-    torch.manual_seed(par.seed)
-    torch.cuda.manual_seed(par.seed)
-    np.random.seed(par.seed)
+    for e in range(par.epochs):
+        spk_plot[b].append(spk[e][b])
         
-    'set model'
-    neuron = models.NeuronClass(par)
-    neuron = funs_train.initialize_weights_PyTorch(par,neuron)
-    
-    if par.noise == False:
-        x_data = funs.get_multisequence(par,timing)
-        w,v,spk,loss = funs_train.train_PyTorch(par,neuron,x=x_data)
-    
-    else: 
-        w,v,spk,loss = funs_train.train_PyTorch(par,neuron,timing=timing)
-        
-    # par.savedir = '/mnt/hpc/departmentN4/matteo_data/predictive_neuron/sequences/'
-    # par.loaddir = '/mnt/hpc/departmentN4/matteo_data/predictive_neuron/sequences/'
-    
-    # np.save('v_tau_{}_vth_{}_rep_{}'.format(par.tau_m,par.v_th,par.rep),v)
-    # np.save('w_tau_{}_vth_{}_rep_{}'.format(par.tau_m,par.v_th,par.rep),w)
-    # np.save('spk_tau_{}_vth_{}_rep_{}'.format(par.tau_m,par.v_th,par.rep),spk)
-    # np.save('loss_tau_{}_vth_{}_rep_{}'.format(par.tau_m,par.v_th,par.rep),loss)
-    
-    '--------------------'
+    fig = plt.figure(figsize=(7,6), dpi=300)
+    for k,j in zip(spk_plot[b],range(par.epochs)):
+        plt.scatter([j]*len(k),np.array(k)-par.onset_list[j]*par.dt,c='rebeccapurple',s=2)
+    plt.ylabel(r'time [ms]')
+    plt.ylim(0,par.T*par.dt)
+    plt.xlim(0,par.epochs)
+    plt.xlabel('epochs')
+    fig.tight_layout(rect=[0, 0.01, 1, 0.97])
+    plt.savefig(os.getcwd()+'/spk_seq_{}.png'.format(b), format='png', dpi=300)
+    # plt.savefig(os.getcwd()+'/spk_seq_{}.pdf'.format(b), format='pdf', dpi=300)
+    plt.close('all')
+
+'Panel c'
+w_plot = np.vstack(w)
+fig = plt.figure(figsize=(7,6), dpi=300)
+plt.title(r'$\vec{w}/\vec{w}_0$')
+plt.ylabel(r'inputs')
+plt.xlabel(r'epochs')
+plt.xlim(0,par.epochs)
+plt.ylim(0,par.N)
+plt.imshow(np.fliplr(w_plot).T/par.init_mean,aspect='auto',cmap='coolwarm',norm=MidpointNormalize(midpoint=1))
+plt.colorbar()
+fig.tight_layout(rect=[0, 0.01, 1, 0.97])
+plt.savefig(os.getcwd()+'/w.png', format='png', dpi=300)
+# plt.savefig(os.getcwd()+'/w.pdf', format='pdf', dpi=300)
+plt.close('all')
+
+
+
+#%%
+w_plot = np.vstack(w)
+plt.imshow(w_plot,aspect='auto')
+
+#%%
+b = 0
+
+
+spk_plot = [[] for b in range(par.batch)]
+for b in range(par.batch):
+    for e in range(par.epochs):
+        spk_plot[b].append(spk[e][b])
+
+#%%
+b = 2
+for k,j in zip(spk_plot[b],range(par.epochs)):
+    plt.scatter([j]*len(k),np.array(k),c='rebeccapurple',s=2)
+plt.ylabel(r'time [ms]')
+plt.ylim(0,par.T*par.dt)
+plt.xlim(0,par.epochs)
+plt.xlabel('epochs')
